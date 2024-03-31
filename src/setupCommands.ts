cc310e376fb55eb7c09b5187420ca106d83c7128
변찬우 <byun00106@gmail.com>
2024-04-01 00:57:00 +0900

    fix: setup command api

--- src/setupCommands.ts
+++ src/setupCommands.ts
@@ -9,15 +9,9 @@ const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN || '')
 
 const setupCommands = async () => {
   try {
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID || '',
-        process.env.GUILD_ID || '',
-      ),
-      {
-        body: commandDatas,
-      },
-    )
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID || ''), {
+      body: commandDatas,
+    })
   } catch (e) {
     console.log(e)
   }
