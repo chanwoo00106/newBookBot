81a5270df5302ed96d61624ca7d2828cac65023c
변찬우 <byun00106@gmail.com>
2024-03-31 22:45:31 +0900

    feat: add prisma schema

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -1,9 +1,13 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     TTB_KEY: string
+
     BOT_TOKEN: string
     NODE_ENV: 'development' | 'production'
     CLIENT_ID: string
     GUILD_ID: stirng
+
+    DATABASE_URL: string
+    DIRECT_URL: string
   }
 }
