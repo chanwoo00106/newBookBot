43e571fec49ed8c205865e38b4ed570189e2f18f
변찬우 <byun00106@gmail.com>
2024-04-04 19:29:25 +0900

    feat: update ping pong test

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,22 @@
-import { SlashCommandBuilder, CommandInteraction } from 'discord.js'
+import {
+  SlashCommandBuilder,
+  CommandInteraction,
+  EmbedBuilder,
+} from 'discord.js'
 import CommandType from './CommandType'
+import dayjs from 'dayjs'
 
 const ping: CommandType = {
   data: new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
 
   execute: async (interaction: CommandInteraction) => {
-    interaction.reply(`Pong! ${interaction.user.username}`)
+    const embed = new EmbedBuilder()
+      .setTitle(`Pong! ${interaction.user.displayName}`)
+      .setDescription('Ping Pong test!')
+      .setColor('#3498DB')
+      .setTimestamp(dayjs().toDate())
+
+    interaction.reply({ embeds: [embed] })
   },
 }
 
