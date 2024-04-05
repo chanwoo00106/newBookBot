f3ba8b9ef186387bb9413f1404e04085154c9035
변찬우 <byun00106@gmail.com>
2024-04-05 16:46:36 +0900

    feat: add book search feature

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,5 +1,19 @@
-import { SlashCommandBuilder } from 'discord.js'
+import { EmbedBuilder, SlashCommandBuilder } from 'discord.js'
 import CommandType from './CommandType'
+import commandOptionToJson from '../libs/commandOptionToJson'
+import { z } from 'zod'
+import searchBook from '../service/searchBook'
+import { createBookEmbed } from '../embeds'
+
+const validation = z.object({
+  query: z.string(),
+  querytype: z.string().optional().default('keyword'),
+  searchtarget: z.string().optional().default('book'),
+  start: z.number().optional().default(1),
+  maxresults: z.number().optional().default(10),
+  sort: z.string().optional().default('accuracy'),
+  cover: z.string().optional().default('none'),
+})
 
 const search: CommandType = {
   data: () => {
@@ -8,8 +22,11 @@ const search: CommandType = {
       .setName('search')
       .setDescription('책 검색')
       .addStringOption((option) =>
+        option.setName('query').setDescription('검색어').setRequired(true),
+      )
+      .addStringOption((option) =>
         option
-          .setName('querytype')
+          .setName('query-type')
           .setDescription('검색 옵션')
           .addChoices({ name: '제목+저자', value: 'keyword' })
           .addChoices({ name: '제목 검색', value: 'title' })
@@ -18,17 +35,71 @@ const search: CommandType = {
       )
       .addStringOption((option) =>
         option
-          .setName('searchtarget')
+          .setName('search-target')
           .setDescription('검색 타겟')
-          .addChoices({ name: '도서', value: 'book' }),
+          .addChoices({ name: '도서', value: 'book' })
+          .addChoices({ name: '외국도서', value: 'foreign' })
+          .addChoices({ name: '음반', value: 'music' })
+          .addChoices({ name: 'DVD', value: 'dvd' })
+          .addChoices({ name: '중고샵(도서/음반/DVD 등)', value: 'used' })
+          .addChoices({ name: '전자책', value: 'ebook' })
+          .addChoices({ name: '전부', value: 'all' }),
+      )
+      .addNumberOption((option) =>
+        option
+          .setName('start')
+          .setDescription('검색결과 시작페이지')
+          .setMinValue(1),
+      )
+      .addNumberOption((option) =>
+        option
+          .setName('max-results')
+          .setDescription('검색결과 한 페이지당 최대 출력 개수')
+          .setMinValue(1)
+          .setMaxValue(100),
+      )
+      .addStringOption((option) =>
+        option
+          .setName('sort')
+          .setDescription('정렬 순서')
+          .addChoices({ name: '(기본값) 관련도', value: 'accuracy' })
+          .addChoices({ name: '출간일', value: 'publishtime' })
+          .addChoices({ name: '제목', value: 'title' })
+          .addChoices({ name: '판매량', value: 'salespoint' })
+          .addChoices({ name: '고객 평점', value: 'customerrating' })
+          .addChoices({ name: '마이리뷰갯수', value: 'myreviewcount' }),
+      )
+      .addStringOption((option) =>
+        option
+          .setName('cover')
+          .setDescription('표지 이미지 크기')
+          .addChoices({ name: '큰 크기 : 너비 200px', value: 'big' })
+          .addChoices({ name: '중간 큰 크기 : 너비 150px', value: 'midbig' })
+          .addChoices({ name: '중간 크기 : 너비 85px', value: 'mid' })
+          .addChoices({ name: '작은 크기 : 너비 75px', value: 'small' })
+          .addChoices({ name: '매우 작은 크기 : 너비 65px', value: 'mini' })
+          .addChoices({ name: '(기본값)없음', value: 'none' }),
       )
 
     return command
   },
   execute: async (interaction) => {
-    console.log(interaction.options.data)
+    const value = commandOptionToJson<z.infer<typeof validation>>(
+      [...interaction.options.data],
+      validation,
+    )
 
-    interaction.reply('test')
+    if (!value) return interaction.reply('입력 타입이 올바르지 않습니다')
+
+    const result = await searchBook(value)
+
+    let embeds = result.item.map((item) => createBookEmbed(item))
+
+    if (!embeds.length) {
+      embeds = [new EmbedBuilder().setTitle('도서가 없습니다!').setTimestamp()]
+    }
+
+    interaction.reply({ embeds })
   },
 }
 
