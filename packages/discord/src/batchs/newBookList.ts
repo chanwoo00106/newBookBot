924262785a704938fc835711e92f3ae31b06208d
변찬우 <byun00106@gmail.com>
2024-04-30 10:04:39 +0900

    fix: notify new book logic

--- packages/discord/src/batchs/newBookList.ts
+++ packages/discord/src/batchs/newBookList.ts
@@ -10,21 +10,20 @@ const newBookList: BatchType = {
   execute: async (client) => {
     if (dayjs().format('HH:mm') !== '07:00') return null
 
-    const data = await findTwoBooks()
-
     const channels = (await db.channel.findMany({ select: { id: true } })).map(
       (i) => i.id,
     )
 
-    let embeds = data.map((item) => createBookEmbed(item))
-
-    if (!embeds.length) {
-      embeds = [new EmbedBuilder().setTitle('신간 도서가 없습니다!')]
-    }
-
     const promises = channels.map((i) => {
-      return new Promise(() => {
+      return new Promise(async () => {
         try {
+          const data = await findTwoBooks(i)
+          let embeds = data.map((item) => createBookEmbed(item))
+
+          if (!embeds.length) {
+            embeds = [new EmbedBuilder().setTitle('신간 도서가 없습니다!')]
+          }
+
           updateViewedBooks(data, i)
 
           const channel = client.channels.cache.get(i)
--- packages/discord/src/service/findTwoBooks.ts
+++ packages/discord/src/service/findTwoBooks.ts
@@ -1,7 +1,12 @@
 import db from '../db'
 
-const findTwoBooks = async () => {
+const findTwoBooks = async (channelId: string) => {
   return db.book.findMany({
+    where: {
+      NOT: {
+        channels: { some: { channelId } },
+      },
+    },
     orderBy: [{ id: 'desc' }],
     take: 2,
   })
