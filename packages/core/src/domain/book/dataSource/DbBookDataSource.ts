a40d01eb039588dab155f814bbed8a1d08d854d0
변찬우 <byun00106@gmail.com>
2024-05-05 14:46:39 +0900

    feat: fetch two books api

--- packages/core/src/domain/book/dataSource/DbBookDataSource.ts
+++ packages/core/src/domain/book/dataSource/DbBookDataSource.ts
@@ -1,10 +1,23 @@
 import { db } from '@/shared'
 import { AddBookListReqDto } from '../dto/AddBookListReqDto'
+import { FetchTwoBooksResDto } from '../dto/FetchTwoBooksResDto'
 
 class DbBookDataSource {
   async addBookList(books: AddBookListReqDto) {
     db.book.createMany({
       data: books,
+    })
+  }
+
+  async fetchTwoBooks(channelId: string): Promise<FetchTwoBooksResDto> {
+    return db.book.findMany({
+      where: {
+        NOT: {
+          channels: { some: { channelId } },
+        },
+      },
+      orderBy: [{ id: 'desc' }],
+      take: 2,
     })
   }
 }
